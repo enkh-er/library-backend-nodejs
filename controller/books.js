73aff4004b9ab3dc7e2194a80ceceebb6f17faf0
EnkhErdene0421 <enkhgem@gmail.com>
2021-01-18 18:43:10 +0800

    merge

---
+++ controller/books.js
@@ -0,0 +1,24 @@
+const Book = require("../models/Book");
+const MyError = require("../utils/myError");
+const asyncHandler = require("express-async-handler");
+
+//api/v1/books
+//api/v1/categories/:catId/books
+exports.getBooks = asyncHandler(async (req, res, next) => {
+  let query;
+  if (req.params.categoryId) {
+    query = Book.find({ category: req.params.categoryId });
+  } else {
+    query = Book.find().populate({
+      path: "category",
+      select: "name averagePrice",
+    });
+  }
+
+  const books = await query;
+  res.status(200).json({
+    success: true,
+    count: books.length,
+    data: books,
+  });
+});
